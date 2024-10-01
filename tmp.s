4d524b95a5e1d72c48e62ee3e989beb19d47231b
Taisei Ide <taisei8955@gmail.com>
2024-10-01 22:42:53 +0900

    Allows multi charracter variables to be defined

--- tmp.s
+++ tmp.s
@@ -2,20 +2,21 @@
 main:
   push %rbp
   mov %rsp, %rbp
-  sub $208, %rsp
-  lea -8(%rbp), %rax
-  push %rax
+  sub $16, %rsp
   lea -16(%rbp), %rax
   push %rax
   mov $3, %rax
   pop %rdi
   mov %rax, (%rdi)
+  lea -8(%rbp), %rax
+  push %rax
+  mov $5, %rax
   pop %rdi
   mov %rax, (%rdi)
-  lea -16(%rbp), %rax
+  lea -8(%rbp), %rax
   mov (%rax), %rax
   push %rax
-  lea -8(%rbp), %rax
+  lea -16(%rbp), %rax
   mov (%rax), %rax
   pop %rdi
   add %rdi, %rax
