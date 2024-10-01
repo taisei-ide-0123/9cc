39bac3d434f40389483ee99e460af987acd8f257
Taisei Ide <taisei8955@gmail.com>
2024-10-01 23:08:36 +0900

    Adds return statement

--- tmp.s
+++ tmp.s
@@ -2,24 +2,12 @@
 main:
   push %rbp
   mov %rsp, %rbp
-  sub $16, %rsp
-  lea -16(%rbp), %rax
-  push %rax
+  sub $0, %rsp
+  mov $1, %rax
+  mov $2, %rax
   mov $3, %rax
-  pop %rdi
-  mov %rax, (%rdi)
-  lea -8(%rbp), %rax
-  push %rax
-  mov $5, %rax
-  pop %rdi
-  mov %rax, (%rdi)
-  lea -8(%rbp), %rax
-  mov (%rax), %rax
-  push %rax
-  lea -16(%rbp), %rax
-  mov (%rax), %rax
-  pop %rdi
-  add %rdi, %rax
+  jmp .L.return
+.L.return:
   mov %rbp, %rsp
   pop %rbp
   ret
